5722ddacaec4469dba9b44dbd1758f582863fab1
yurizzxz <yuri.alves.silva@gmail.com>
2025-02-03 19:23:41 -0300

    doc: corrigindo textos

--- src/app/_sections/plans/plans.tsx
+++ src/app/_sections/plans/plans.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Planos() {
   const planos = [
@@ -8,7 +7,7 @@ export default function Planos() {
       title: "Plano Standart",
       description: "A solução ideal para quem deseja dar os primeiros passos.",
       price: 119.9,
-      monthlyPrice: 149.9,
+      monthlyPrice: 149.90,
       highlighted: false,
       url: "https://abrir.link/sKZeo",
       benefits: [
@@ -17,7 +16,7 @@ export default function Planos() {
         "Assistência técnica 6/7",
         "Cadastro de 10 Presets de Treinos",
         "Liberação parcial do Dashboard",
-        "Até 200 alunos cadastrados",
+        "Até 300 a 1000 alunos cadastrados",
       ],
     },
     {
@@ -25,7 +24,7 @@ export default function Planos() {
       description:
         "Para academias em crescimento, com mais recursos e suporte.",
       price: 219.9,
-      monthlyPrice: 265.9,
+      monthlyPrice: 265.90,
       highlighted: true,
       url: "https://abrir.link/UKhhc",
       benefits: [
@@ -35,7 +34,7 @@ export default function Planos() {
         "Cadastro de 25 Presets de Treinos",
         "Acompanhamento de resultados",
         "Liberação total ao Dashboard",
-        "Até 500 alunos cadastrados",
+        "Até 1000 a 2000 alunos cadastrados",
       ],
     },
     {
@@ -43,7 +42,7 @@ export default function Planos() {
       description:
         "O plano definitivo para academias que buscam excelência e resultados.",
       price: 380.9,
-      monthlyPrice: 459.9,
+      monthlyPrice: 459.90,
       url: "https://abrir.link/XSmna",
       highlighted: false,
       benefits: [
@@ -52,12 +51,10 @@ export default function Planos() {
         "Assistência técnica 12/7",
         "Cadastro ilimitado de Presets de Treinos",
         "Liberação total ao Dashboard",
-        "De 500 a 1000 alunos cadastrados",
+        "Negociar quanitdade de alunos cadastrados",
       ],
     },
   ];
-
-  const [isAnual, setIsAnual] = useState(false);
 
   return (
     <section
@@ -65,7 +62,7 @@ export default function Planos() {
       id="plans"
     >
       <div className="container mx-auto items-center px-5">
-        <header className="lg:text-center mb-8 mt-28 lg:mt-16">
+        <header className="lg:text-center mb-12 mt-28 lg:mt-16">
           <h1 className="text-5xl lg:text-6xl font-bold">
             Seja nosso <span className="text-[#00bb83]">parceiro</span>!
           </h1>
@@ -74,33 +71,8 @@ export default function Planos() {
           </p>
         </header>
 
-        <div className="flex justify-center gap-4 mb-16">
-          <button
-            className={`px-8 py-3 rounded-lg ${
-              !isAnual
-                ? "bg-[#00bb83]"
-                : "bg-[#252525] text-white border border-[#252525]"
-            }`}
-            onClick={() => setIsAnual(false)}
-          >
-            Mensal
-          </button>
-          <button
-            className={`px-8 py-3 rounded-lg ${
-              isAnual
-                ? "bg-[#00bb83]"
-                : "bg-[#252525] text-white border border-[#252525]"
-            }`}
-            onClick={() => setIsAnual(true)}
-          >
-            Anual
-          </button>
-        </div>
-
         <div className="container mx-auto lg:px-4 flex justify-center flex-wrap gap-8">
           {planos.map((plano, index) => {
-            const valorAjustado = isAnual ? plano.price : plano.monthlyPrice;
-
             return (
               <div
                 key={index}
@@ -118,10 +90,10 @@ export default function Planos() {
                 <div className="mb-5 mt-8">
                   <h2 className="text-4xl font-bold mb-3">{plano.title}</h2>
                   <h2 className="text-5xl font-bold mb-3 text-[#2bd6a3]">
-                    R$ {valorAjustado.toFixed(2)}/mês
+                    R$ {plano.price}/mês
                   </h2>
                   <p className="text-sm font-bold mb-3 text-gray-400">
-                   Total de R$ {(valorAjustado * 12).toFixed(2)}/ano
+                    Total de R$ {(plano.price * 12).toFixed(2)}/ano
                   </p>
                   <p className="text-md mb-8">{plano.description}</p>
 
